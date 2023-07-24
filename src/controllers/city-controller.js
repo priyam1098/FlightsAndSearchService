f676377080d2e0286be04ce6616e20a4028d8b5a
Priyam1098 <priyamgupta1098@gmail.com>
2023-07-24 14:55:14 +0530

    Added filter to get all cities

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -81,7 +81,7 @@ const update = async (req, res) => {
 };
 const getAll = async (req, res) => {
   try {
-    const city = await cityService.getAllCities();
+    const city = await cityService.getAllCities(req.query);
     return res.status(200).json({
       data: city,
       success: true,
